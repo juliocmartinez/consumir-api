0ff4b27098137d498cac42e8ef5214311f0ad305
juliocmartinez <julio.martinezg8@gmail.com>
2021-12-09 16:13:06 -0500

    version 1

---
+++ api.js
@@ -0,0 +1,41 @@
+/*const realizarPeticion=async()=>{
+    const res= await fetch('https://pokeapi.co/api/v2/pokemon/pikachu/');
+    const data = await res.json();
+    console.log(data);
+}
+realizarPeticion();*/
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+const pokemonContainer = document.querySelector(".pokemon-container");
+
+button.addEventListener("click",(e)=>{
+    e.preventDefault();
+    traerPokemon(input.value);
+});
+
+function traerPokemon(pokemon) {
+fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+        .then((res) => res.json())
+        .then((data) => {
+            crearPokemon(data);
+        });
+}
+
+function crearPokemon(pokemon) {
+    const img = document.createElement('img');
+    img.src = pokemon.sprites.front_default;
+
+    const h3 = document.createElement('h3');
+    h3.textContent = pokemon.name;
+
+    const div = document.createElement('div');
+    div.appendChild(img);
+    div.appendChild(h3);
+    pokemonContainer.appendChild(div);
+
+
+}
+
+traerPokemon();
+
